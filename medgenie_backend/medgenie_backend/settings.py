6a51dd8f0957ac0df003e188096a6b45b775fcea
martian3062 <116432908+martian3062@users.noreply.github.com>
2025-12-28 22:39:09 +0530

    Add Session and Basic Authentication to REST framework

--- medgenie_backend/medgenie_backend/settings.py
+++ medgenie_backend/medgenie_backend/settings.py
@@ -67,7 +67,8 @@ AUTH_PASSWORD_VALIDATORS = []
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     )
 }
 
